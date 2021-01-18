e122bcd4b876ac9f08a29c661845bcc15b2e2a4b
Joseph Domenici <josephdomenici@gmail.com>
2021-01-18 01:24:04 -0800

    P calculation fixes; CSS tweaking

--- assets/scripts/prd.js
+++ assets/scripts/prd.js
@@ -1,17 +1,135 @@
+var sliderPFromC;
+var sliderCFromP;
+var inputPFromC;
+var inputCFromP;
+var chartP;
+var chartC;
+
+window.onload = function()
+{
+	var sliders = document.querySelectorAll("input[type=range]");
+	for (var i = 0; i < sliders.length; i++) { sliders[i].addEventListener('input', updateValue); }
+	
+	var inputs = document.querySelectorAll("input[type=text]");
+	for (var i = 0; i < inputs.length; i++) { inputs[i].addEventListener('change', updateValue); }
+	
+	sliderPFromC = document.getElementById("slider-p-from-c");
+	sliderCFromP = document.getElementById("slider-c-from-p");
+	inputPFromC = document.getElementById("input-p-from-c");
+	inputCFromP = document.getElementById("input-c-from-p");
+	
+	createChartP();
+	createChartC();
+}
+
+function createChartP()
+{
+	var p = calculatePFromC(sliderPFromC.value);
+	
+	chartP = new Chart(document.getElementById("chart-p").getContext('2d'), {
+		type: "line",
+		data: {
+			labels: p.trials,
+			datasets: [{
+				label: "% per trial",
+				data: p.data,
+				borderColor: "#3e95cd",
+				fill: false
+			}, {
+				label: "Cumulative %",
+				data: p.cumData,
+				borderColor: "#8e5ea2",
+				fill: false
+			}]
+		},
+		options: {
+			title: {
+				display: true,
+				text: "(P) = " + (Math.round(p.value * 10000) / 100).toFixed(2) + "%"
+			}
+		}
+	});
+}
+
+function createChartC()
+{
+	// chartC = ....
+}
+
+function updateValue(e)
+{
+	// when updating chart, display the real value being calculated for (restrict to between min-max, but don't mess with input field)
+	switch (e.target.id)
+	{
+		case "slider-p-from-c":
+			inputPFromC.value = sliderPFromC.value;
+			simulateP();
+			break;
+		case "slider-c-from-p":
+			inputCFromP.value = sliderCFromP.value;
+
+			break;
+		case "input-p-from-c":
+			inputPFromC.value = inputPFromC.value.replace(/[^\d.]/g, "");
+			
+			if (inputPFromC.value === "NaN" || inputPFromC.value <= 0)
+			{
+				inputPFromC.value = inputPFromC.defaultValue;
+			}
+			
+			sliderPFromC.value = inputPFromC.value;
+			simulateP();
+			
+			break;
+		case "input-c-from-p":
+			
+			
+			break;
+		default:
+			alert("You broke me. Please contact josephdomenici@gmail.com!");
+			return;
+	}
+}
+
 function simulateP()
 {
 	var c = 0;
 	
 	try
 	{
-		c = parseFloat(document.getElementById("c").value);
+		c = parseFloat(document.getElementById("input-p-from-c").value);
 	}
 	catch (err)
 	{
 		alert("Number must be between 0 and 1");
 	}
 	
-	document.getElementById("p-result").innerHTML = calculatePFromC(c);
+	var p = calculatePFromC(c);
+	
+	chartP.data.labels = p.trials;
+	chartP.data.datasets = [{
+		label: "% per trial",
+		data: p.data,
+		borderColor: "#3e95cd",
+		fill: false
+	}, {
+		label: "Cumulative %",
+		data: p.cumData,
+		borderColor: "#8e5ea2",
+		fill: false
+	}];
+	chartP.options = {
+		title: {
+			display: true,
+			position: 'top',
+			text: "(P) = " + (Math.round(p.value * 10000) / 100).toFixed(2) + "%"
+		}
+	};
+	
+	chartP.update({
+		duration: 0,
+		easing: "linear"
+	});
 }
 
 function simulateC()
@@ -20,7 +138,7 @@ function simulateC()
 	
 	try
 	{
-		p = parseFloat(document.getElementById("p").value);
+		p = parseFloat(document.getElementById("c-from-p").value);
 	}
 	catch (err)
 	{
@@ -32,16 +150,43 @@ function simulateC()
 
 function calculatePFromC(c)
 {
-	var sum = 0;
+	var p = { value: -1, data: [], cumData: [], trials: [] }, sum = 0;
 	
-	for (var n = 1, pByN = 0; c * (n - 1) <= 1; n++)
+	for (var n = 1, pByN = 0; c * (n - 1) < 1; n++)
 	{
 		var pOnN = Math.min(1, c * n) * (1 - pByN);
 		pByN += pOnN;
 		sum += n * pOnN;
+		
+		p.data.push(pOnN);
+		// p.cumData.push(pByN);
+		p.trials.push(n);
 	}
 	
-	return (1 / sum);
+	p.cumData = calculateCumP(c);
+	p.value = 1 / sum;
+	
+	return p;
+}
+
+function calculateCumP(c)
+{
+	var cumData = [];
+	
+	for (var n = 0; c * n < 1; n++)
+	{
+		if (n == 0)
+		{
+			cumData.push(c);
+		}
+		else
+		{
+			var cumP = 1 - ((1 - (c * (n + 1))) * (1 - cumData[n - 1]));
+			cumData.push(Math.min(1, cumP));
+		}
+	}
+	
+	return cumData;
 }
 
 function calculateCFromP(p)
